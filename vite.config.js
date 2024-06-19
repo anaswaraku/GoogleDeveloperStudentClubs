af5aa24cd5b2c9f4b31ee3b79268a1349d3e013f
Anaswara K U <100979994+anaswaraku@users.noreply.github.com>
2024-06-19 11:06:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "GoogleDeveloperStudentClubs",
+  base: "./",
   plugins: [react()],
 });
